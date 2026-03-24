64b601c5c910e7702954b4eb38e0cf14602ccdb8
haxllo <mshabeeburrahman786@gmail.com.com>
2026-03-24 15:07:53 +0530

    fix(serial): disable absent uart input

--- include/kernel/serial.h
+++ include/kernel/serial.h
@@ -3,6 +3,7 @@
 
 void serial_init(void);
 void serial_write_char(char ch);
+int serial_available(void);
 int serial_can_read(void);
 char serial_read_char(void);
 
--- src/kernel/serial.c
+++ src/kernel/serial.c
@@ -8,26 +8,57 @@ enum {
     COM1 = 0x3F8,
 };
 
+static int serial_present;
+
 static int serial_tx_ready(void) {
+    if (!serial_present) {
+        return 0;
+    }
+
     return (inb(COM1 + 5) & 0x20) != 0;
 }
 
+int serial_available(void) {
+    return serial_present;
+}
+
 int serial_can_read(void) {
+    if (!serial_present) {
+        return 0;
+    }
+
     return (inb(COM1 + 5) & 0x01) != 0;
 }
 
 void serial_init(void) {
+    uint8_t probe_value;
+
     outb(COM1 + 1, 0x00);
     outb(COM1 + 3, 0x80);
     outb(COM1 + 0, 0x03);
     outb(COM1 + 1, 0x00);
     outb(COM1 + 3, 0x03);
     outb(COM1 + 2, 0xC7);
-    outb(COM1 + 4, 0x0B);
+    outb(COM1 + 4, 0x1Eu);
+    outb(COM1 + 0, 0xAEu);
     io_wait();
+
+    probe_value = inb(COM1 + 0);
+    if (probe_value != 0xAEu) {
+        serial_present = 0;
+        outb(COM1 + 4, 0x00u);
+        return;
+    }
+
+    serial_present = 1;
+    outb(COM1 + 4, 0x0Bu);
 }
 
 void serial_write_char(char ch) {
+    if (!serial_present) {
+        return;
+    }
+
     while (!serial_tx_ready()) {
     }
 
@@ -35,6 +66,10 @@ void serial_write_char(char ch) {
 }
 
 char serial_read_char(void) {
+    if (!serial_present) {
+        return '\0';
+    }
+
     while (!serial_can_read()) {
     }
 
